ad55676a27fcb622b4323b2a28f09c57a809685d
Mika Hyttinen <mika.hyttinen@gmail.com>
2025-12-31 18:42:18 +0300

    chore: Remove unused DAP_DEL_MULTY macro

--- crypto/cellframe_dilithium/dap_crypto_common.h
+++ crypto/cellframe_dilithium/dap_crypto_common.h
@@ -14,12 +14,6 @@ typedef unsigned char byte_t;
 #define DAP_NEW_Z(type) ((type*)calloc(1, sizeof(type)))
 #define DAP_NEW_Z_SIZE(type, size) ((type*)calloc(1, (size)))
 #define DAP_DEL_Z(ptr) do { if (ptr) { free(ptr); (ptr) = NULL; } } while(0)
-#define DAP_DEL_MULTY(...) do { \
-    void *_ptrs[] = {__VA_ARGS__}; \
-    for (size_t _i = 0; _i < sizeof(_ptrs)/sizeof(void*); _i++) { \
-        if (_ptrs[_i]) free(_ptrs[_i]); \
-    } \
-} while(0)
 
 // DAP utility macros - compatibility layer for Cellframe framework
 #define dap_return_if_pass(cond) do { if (cond) return; } while(0)
