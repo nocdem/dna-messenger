a2ddae6aa330cccaa064275f417cac142f8c85ba
Mika Hyttinen <mika.hyttinen@gmail.com>
2026-01-02 10:06:18 +0200

    New logo again

--- dna_messenger_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ dna_messenger_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import emoji_picker_flutter
+import file_picker
 import file_selector_macos
 import package_info_plus
 import path_provider_foundation
@@ -17,6 +18,7 @@ import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   EmojiPickerFlutterPlugin.register(with: registry.registrar(forPlugin: "EmojiPickerFlutterPlugin"))
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
