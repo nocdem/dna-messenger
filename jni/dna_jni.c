335410e8068181c3486e9b50c0bbc23115a06551
Mika Hyttinen <mika.hyttinen@gmail.com>
2026-01-06 19:29:13 +0300

    fix: Add missing dht_singleton.h include in dna_jni.c

--- jni/dna_jni.c
+++ jni/dna_jni.c
@@ -15,6 +15,7 @@
 #include <android/log.h>
 
 #include "dna/dna_engine.h"
+#include "dht/client/dht_singleton.h"
 
 #define LOG_TAG "DNA-JNI"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
